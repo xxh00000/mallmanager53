13155c4a77a02f0b5d97d4a7b959cd7e31a25718
xxh00000 <1578575212@qq.com>
2020-12-07 15:52:30 +0800

    manage

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-07 12:36:20
+ * @LastEditTime: 2020-12-07 15:09:17
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \mallmanager53\src\App.vue
+-->
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-07 12:36:20
+ *  stEditTime: 2020-12-07 15:10:00
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \mallmanager53\src\router\index.js
+ */
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/'
+    }
+  ]
+})
