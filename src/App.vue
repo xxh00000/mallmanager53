d5401e192b6ff9fde1a1e0733a1da98b8f102af6
xxh00000 <1578575212@qq.com>
2020-12-08 10:01:59 +0800

    login

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <!--
  * @Author: your name
  * @Date: 2020-12-07 12:36:20
- * @LastEditTime: 2020-12-07 15:09:17
+ * @LastEditTime: 2020-12-08 09:59:42
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \mallmanager53\src\App.vue
 -->
 <template>
   <div id="app">
+    <router-link to="/login">登录</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
---
+++ src/components/login/login.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-12-07 16:44:25
+ * @LastEditTime: 2020-12-08 09:50:02
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \mallmanager53\src\components\login\login.vue
+-->
+<template>
+  <div>
+    denglu
+  </div>
+</template>
+
+<script>
+export default { }
+</script>
+
+<style>
+</style>
